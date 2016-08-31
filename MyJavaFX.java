5efd7e632dac6b2509051d7b5ee5fd4d960f6565
island <1159401236@qq.com>
2016-08-31 09:51:39 +0800

    good morning

--- MyJavaFX.java
+++ MyJavaFX.java
@@ -8,8 +8,8 @@ public class MyJavaFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         // Create a button and place it in the scene
-        Button btOK = new Button("OK");
-        Scene scene = new Scene(btOK, 200, 250);
+        // Button btOK = new Button("OK");
+        Scene scene = new Scene(new Button("OK"), 200, 250);
         primaryStage.setTitle("MyJavaFX"); // Set the stage title
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the statge
